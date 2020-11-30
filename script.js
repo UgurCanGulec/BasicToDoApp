62c661ec4352e233bbc3fbe9231e062dd7d0b41c
UgurCanGulec <gulecugurcan@gmail.com>
2020-11-30 13:23:26 +0300

    UI varriables were defined

---
+++ script.js
@@ -0,0 +1,6 @@
+//UI vars
+
+const form=document.querySelector("#addTaskForm");
+const input=document.querySelector("#txtTaskName");
+const btnDeleteAll=document.querySelector("#btnDeleteAll");
+const taskList=document.querySelector("#task-list");
